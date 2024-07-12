432278957e9911d402307a2dc8b1df94cd15d7fe
Pouya Mohammadi <pouya.mohammadi@gauss-robotics.de>
2024-07-12 09:43:19 +0200

    some w.i.p for testing

--- bindings/python/tasks/velocity.hpp
+++ bindings/python/tasks/velocity.hpp
@@ -58,9 +58,14 @@ void pyVelocityCartesian(py::module& m) {
           .def_property_readonly("baseLinkIsWorld", &Cartesian::baseLinkIsWorld);
 }
 
-void pyVelocityCartesianAdmittance(py::module & m){
-    py::class_<CartesianAdmittance, std::shared_ptr<CartesianAdmittance>, Task<Eigen::MatrixXd, Eigen::VectorXd>>(m, "CartesianAdmittance")
-          .def(py::init<std::string, XBot::ModelInterface&, std::string, XBot::ForceTorqueSensor::ConstPtr>());
+void pyVelocityCartesianAdmittance(py::module & m) {
+    py::class_<CartesianAdmittance, std::shared_ptr<CartesianAdmittance>, Cartesian>(m, "CartesianAdmittance")
+          .def(py::init<std::string, XBot::ModelInterface&, std::string, XBot::ForceTorqueSensor::ConstPtr>())
+//          .def("getCartesianCompliance", py::overload_cast<Eigen::Matrix6d&>(&CartesianAdmittance::getCartesianCompliance))
+//          .def("getCartesianCompliance",&CartesianAdmittance::getCartesianCompliance,py::return_value_policy::reference)
+          .def("getCartesianCompliance",&CartesianAdmittance::getCartesianCompliance)
+          .def("setWrenchReference", py::overload_cast<const Eigen::Vector6d&>(&CartesianAdmittance::setWrenchReference));
+//          .def("getWrenchReference", &CartesianAdmittance::getWrenchReference);
 
 }
 
